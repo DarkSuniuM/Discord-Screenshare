03e5e8d557aeb3cd9f7f42476685747ebe864c6b
Alireza Ayinmehr <alireza.darksun@gmail.com>
2022-09-01 22:11:46 +0430

    refactor: remove unneeded features

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 require("dotenv").config();
 import Discord from "discord.js-selfbot-v13";
 import COMMANDS from "./commands";
-import { contextFactory, notAllowed } from "./commands/_utils";
-import { OWNER_ID, PREFIX, REJECT, TOKEN } from "./config";
+import { contextFactory } from "./commands/_utils";
+import { OWNER_ID, PREFIX, TOKEN } from "./config";
 import { Stream } from "./stream";
-let users = require("./users.json");
 
-let intLoop: NodeJS.Timer | undefined;
-let loop = false;
 const client = new Discord.Client();
 
 let stream = new Stream(TOKEN);
@@ -20,34 +17,12 @@ client.on("messageCreate", (msg) => {
   if (!msg.content.startsWith(PREFIX)) return;
   const { command, ...context } = contextFactory(stream, msg);
 
-  if (OWNER_ID && !users.includes(msg.author.id) && msg.author.id != OWNER_ID) return;
-
+  if (OWNER_ID && msg.author.id != OWNER_ID) return;
   const fn = COMMANDS[command];
   if (fn) {
     return fn({ command, ...context });
   }
-  switch (command) {
-    case "loop":
-      if (notAllowed(stream, msg)) msg.react(REJECT);
-      else {
-        if (!loop) {
-          loop = true;
-          intLoop = setInterval(() => {
-            stream.current()?.then((result: any) => {
-              if (result >= stream.duration) stream.driver?.executeScript("video.play()");
-            });
-          }, 100);
-          msg.reply("Video loop set");
-        } else {
-          loop = false;
-          clearInterval(intLoop);
-          msg.reply("Video loop unset");
-        }
-      }
-      break;
-    default:
-      msg.reply("Unknown command, type `*help` for list of commands");
-  }
+  msg.reply("Unknown command, type `*help` for list of commands");
 });
 
 client.login(TOKEN);
